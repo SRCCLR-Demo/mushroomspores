ce79b24b838afd3d6d9cbf30ff2887270c3bd508
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2016-06-19 10:13:07 -0400

    store and update a single value

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+currid = [{ 'currid': 1 }]
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/todo/api/v1.0/currid', methods=['GET'])
+def get_id():
+	return jsonify({'currid': currid})
+
+@app.route('/todo/api/v1.0/currid', methods=['POST'])
+def post_id():
+	if not request.json:
+		print "could not find request.json"
+	if not 'newid' in request.json:
+		print "double fuck me like a rack of boar"
+	print "okay this should fucking work now"
+	print request.json
+	currid[0]['currid'] = request.json['newid']
+	print currid
+	return jsonify({'currid': currid}), 201
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
