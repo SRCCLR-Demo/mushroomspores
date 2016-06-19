76f0441da0fb5bdec5c8a52a2b4f7431b3b8e6e6
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2016-06-18 22:25:08 -0400

    trying adding route

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-curr_id = [{ 'curr_id': 1 }]
+currid = [{ 'currid': 1 }]
 @app.route('/')
 def hello():
     return 'Hello World!'
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/todo/api/v1.0/currid', methods=['GET'])
 def get_id():
-	return jsonify({'curr_id': curr_id})
+	return jsonify({'currid': currid})
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
