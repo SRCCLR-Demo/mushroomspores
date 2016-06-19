e0b0c9cfd46be9ab178a054929de256a6b750103
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2016-06-18 22:19:12 -0400

    trying adding route

--- app.py
+++ app.py
@@ -3,9 +3,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+curr_id = { 'id': 1 }
+
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/id', methods=['GET'])
+def get_id():
+	return jsonify({'id': curr_id})
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
